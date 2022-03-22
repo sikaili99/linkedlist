29777520857c9aa780a6fdbb85f2e40e777300bc
Mathews Musukuma <33897492+Mathewsmusukuma@users.noreply.github.com>
2022-03-22 23:50:13 +0000

    Insert at the end of LinkedList

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ class LinkedList:
     node = Node(data, self.head)
     self.head = node
 
+  def insert_at_end(self, data):
+    if self.head is None:
+      self.head = Node(data, None)
+      return None
+    
+    itr = self.head
+
+    while itr.next:
+      itr = itr.next
+      
+    itr.next = Node(data, None)
+
   def print_list(self):
     if self.head is None:
       print("Empty LinkedList")
@@ -26,8 +38,12 @@ class LinkedList:
 
 if __name__ == "__main__":
   llist = LinkedList()
+
+  llist.insert_at_beginning(8)
+  llist.insert_at_beginning(9)
+  llist.insert_at_beginning(10)
+  llist.insert_at_beginning(11)
+  llist.insert_at_end(7)
   
-  llist.insert_at_beginning(4)
-  llist.insert_at_beginning(5)
   
   llist.print_list()
