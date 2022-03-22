56b045300a723eb136c653d64786d21d3de53405
Mathews Musukuma <33897492+Mathewsmusukuma@users.noreply.github.com>
2022-03-22 22:17:35 +0000

    Basic LinkedList -> insert at beginning -> print data

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 class Node:
-  def __init__(self, data=None):
+  def __init__(self, data=None, next=None):
     self.data = data
     self.next = None
 
 class LinkedList:
   def __init__(self):
     self.head = None
-  def insert(self, data):
-    node = Node(data)
+    
+  def insert_at_beginning(self, data):
+    node = Node(data, self.head)
     self.head = node
 
+  def print_list(self):
+    if self.head is None:
+      print("Empty LinkedList")
+      return
+
+    itr = self.head
+    lstr = " "
+
+    while itr:
+      lstr += f"{itr.data}--->"
+      itr = itr.next
+    print(lstr)
+
+llist = LinkedList()
+
+llist.insert_at_beginning(4)
+llist.insert_at_beginning(5)
+
+llist.print_list()
